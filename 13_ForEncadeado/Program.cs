cb1ee6595f3cec8d1ca5b1e8b8c42e4448ab79b3
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-23 23:36:45 -0300

    Adicionar arquivos de projeto.

---
+++ 13_ForEncadeado/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //escrevendo asteriscos com o Break
+            Console.WriteLine("Executando Projeto 13");
+                for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+              
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+    
+                    Console.Write("*");
+
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+                
+
+                //uma forma diferente de fazer o desenho dos asteriscos
+            Console.ReadLine();
+        }
+    }
+
+}
